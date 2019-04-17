729cc1b1b81b84dbeaeace9a945ec3d67e6b43d1
samnymr <samnymr@gmail.com>
2019-04-17 21:59:16 +0100

    Update carrier_wave.rb

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,12 @@
-require 'carrierwave/orm/activerecord'
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Configuration for Amazon S3
       provider:              'AWS',
       aws_access_key_id:     'AKIAIAVREEXLBVAVEVLQ',
       aws_secret_access_key: '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',
       region:                'eu-west-2',
-  }
-  config.fog_directory  = 'admitme-images'
+    }
+    config.fog_directory = 'admitme-images'
+  end
 end
